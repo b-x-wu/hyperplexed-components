18258d17ec46384d59266eb98f89f64b418464b6
bruce-x-wu <wu.bruce.x@gmail.com>
2023-05-10 15:44:12 -0700

    style: tag animations with component name

--- components/magicalText.tsx
+++ components/magicalText.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react'
 
-
 const starPath = <path d="M512 255.1c0 11.34-7.406 20.86-18.44 23.64l-171.3 42.78l-42.78 171.1C276.7 504.6 267.2 512 255.9 512s-20.84-7.406-23.62-18.44l-42.66-171.2L18.47 279.6C7.406 276.8 0 267.3 0 255.1c0-11.34 7.406-20.83 18.44-23.61l171.2-42.78l42.78-171.1C235.2 7.406 244.7 0 256 0s20.84 7.406 23.62 18.44l42.78 171.2l171.2 42.78C504.6 235.2 512 244.6 512 255.1z" />
 
 interface MagicalTextProps {
@@ -68,8 +67,8 @@ const Star = (props: StarProps) => {
     }, [position])
 
     return (
-        <span className={`block absolute top-[${position.top}%] left-[${position.left}%] h-6 w-6 fill-[${props.color}] opacity-70 ${isAnimated ? `animate-[scale_${props.scaleDuration}ms_ease_forwards]` : 'hidden'}`}> 
-            <svg viewBox="0 0 512 512" className={`animate-[rotate_${props.rotateDuration}ms_linear_infinite]`}>
+        <span className={`block absolute top-[${position.top}%] left-[${position.left}%] h-6 w-6 fill-[${props.color}] opacity-70 ${isAnimated ? `animate-[magicalText-scale_${props.scaleDuration}ms_ease_forwards]` : 'hidden'}`}> 
+            <svg viewBox="0 0 512 512" className={`animate-[magicalText-rotate_${props.rotateDuration}ms_linear_infinite]`}>
                 <path d="M512 255.1c0 11.34-7.406 20.86-18.44 23.64l-171.3 42.78l-42.78 171.1C276.7 504.6 267.2 512 255.9 512s-20.84-7.406-23.62-18.44l-42.66-171.2L18.47 279.6C7.406 276.8 0 267.3 0 255.1c0-11.34 7.406-20.83 18.44-23.61l171.2-42.78l42.78-171.1C235.2 7.406 244.7 0 256 0s20.84 7.406 23.62 18.44l42.78 171.2l171.2 42.78C504.6 235.2 512 244.6 512 255.1z" />
             </svg>
         </span>
@@ -105,15 +104,15 @@ export const MagicalText = ({
     
     // add this to tailwind.config.theme.extend
     // keyframes: {
-    //     'background-pan': {
+    //     'magicalText-background-pan': {
     //         '0%': { 'background-position': '0% center' },
     //         '100%': { 'background-position': '-200% center' },
     //     },
-    //     'scale': {
+    //     'magicalText-scale': {
     //         '0%,100%': { 'transform': 'scale(0)' },
     //         '50%': { 'transform': 'scale(1)' }
     //     },
-    //     'rotate': {
+    //     'magicalText-rotate': {
     //         '0%': { 'transform': 'rotate(0deg)' },
     //         '100%': { 'transform': 'rotate(180deg)' }
     //     }
@@ -124,7 +123,7 @@ export const MagicalText = ({
             <span className='inline-block relative'>
                 {starSvgSpans}
                 <span
-                    className={`bg-gradient-to-r ${gradientClassName} bg-clip-text text-transparent text-inherit bg-[length:200%] animate-[background-pan_${backgroundPanDuration}ms_linear_infinite]`}
+                    className={`bg-gradient-to-r ${gradientClassName} bg-clip-text text-transparent text-inherit bg-[length:200%] animate-[magicalText-background-pan_${backgroundPanDuration}ms_linear_infinite]`}
                 >
                     {children}
                 </span>
