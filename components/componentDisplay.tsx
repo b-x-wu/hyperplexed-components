baa421a661e4a3a4010d7f530ea341ec6f558d96
bruce-x-wu <wu.bruce.x@gmail.com>
2023-05-11 23:47:18 -0700

    feat: add component display to main screen

--- components/componentDisplay.tsx
+++ components/componentDisplay.tsx
@@ -10,27 +10,41 @@ interface ComponentDisplayProps {
     height: string | number
     children: React.ReactNode
     codeText?: string
-    customStyle?: React.CSSProperties
+    containerStyle?: React.CSSProperties
+    title?: string
+    titleHref?: string
+    titleStyle?: React.CSSProperties
 }
 
 export const ComponentDisplay = ({
     width,
     height,
     children,
-    customStyle,
+    containerStyle,
     codeText,
+    title,
+    titleStyle = {},
+    titleHref
 }: ComponentDisplayProps) => {
     const widthString = typeof width === 'string' ? width : `${width}px`
     const heightString = typeof height === 'string' ? height : `${height}px`
+
+    const titleNode = title == null ? <></> :
+        titleHref == null ?
+            <h2 style={titleStyle} className={`w-[${widthString}]`}>{title}</h2> :
+            <h2 style={titleStyle} className={`w-[${widthString}]`}><a href={titleHref}>{title}</a></h2>
     
     return (
-        <div className="flex flex-col md:flex-row gap-y-5 md:gap-x-5">
-            <div style={{ width: widthString, height: heightString, margin: 0, ...customStyle }}>
+        <div className="flex flex-col gap-y-2 items-center">
+            {
+                titleNode ?? <></>
+            }
+            <div style={{ width: widthString, height: heightString, margin: 0, ...containerStyle }}>
                 {children}
             </div>
             {
                 codeText == null ? <></> :
-                <SyntaxHighlighter language="tsx" style={materialDark} customStyle={{ width: widthString, height: heightString, margin: 0, ...customStyle }}>
+                <SyntaxHighlighter language="tsx" style={materialDark} customStyle={{ width: widthString, height: heightString, margin: 0, ...containerStyle }}>
                     {codeText}
                 </SyntaxHighlighter>
             }
--- src/index.tsx
+++ src/index.tsx
@@ -4,30 +4,59 @@ import { FancyGradientHoverLinks } from '../components/fancyGradientHoverLinks'
 import { ComponentDisplay } from '../components/componentDisplay'
 
 const App = () => {
-    const codeString = `import React from 'react'\nconst componentDisplay = () => {\n\treturn <></>\n}`
-    
+    const fancyGradientHoverLinksCodeString = 
+`<div className='bg-[#101020] text-white font-bold text-2xl h-full flex flex-col justify-center font-brunoacesc'>
+  <FancyGradientHoverLinks
+    linkInfos={[
+      {
+        text: "Video",
+        href: "https://www.youtube.com/watch?v=oJYFRZ4cj2Q"
+      },
+      {
+        text: "Channel",
+        href: "https://www.youtube.com/@Hyperplexed"
+      },
+      {
+        text: "CodePen",
+        href: "https://codepen.io/Hyperplexed/pen/wvmvqmx"
+      }
+    ]}
+  />
+</div>`
+
     return (
-        <ComponentDisplay width={500} height={300} codeText={codeString} customStyle={{ borderWidth: 4 }}>
-            <div className='bg-[#101020] text-white font-bold text-3xl h-full flex flex-col justify-center'>
-                <FancyGradientHoverLinks
-                    linkInfos={[
-                        {
-                            text: "link one",
-                            href: "/"
-                        },
-                        {
-                            text: "link too",
-                            href: "/"
-                        },
-                        {
-                            text: "link tree",
-                            href: "/"
-                        }
-                    ]}
-                    borderColor='#ff0000'
-                />
+        <div className='h-max bg-slate-400'>
+            <div className='flex flex-col gap-y-10 max-w-[80%] mx-auto'>
+                <ComponentDisplay
+                    width={500}
+                    height={300}
+                    codeText={fancyGradientHoverLinksCodeString}
+                    containerStyle={{ borderWidth: 4 }}
+                    title={'Fancy Gradient Hover Links'}
+                    titleStyle={{fontSize: '1.5rem', lineHeight: '2rem', color: 'white', fontWeight: 'bold'}}
+                    titleHref='https://www.youtube.com/watch?v=oJYFRZ4cj2Q'
+                >
+                    <div className='bg-[#101020] text-white font-bold text-2xl h-full flex flex-col justify-center font-brunoacesc'>
+                        <FancyGradientHoverLinks
+                            linkInfos={[
+                                {
+                                    text: "Video",
+                                    href: "https://www.youtube.com/watch?v=oJYFRZ4cj2Q"
+                                },
+                                {
+                                    text: "Channel",
+                                    href: "https://www.youtube.com/@Hyperplexed"
+                                },
+                                {
+                                    text: "CodePen",
+                                    href: "https://codepen.io/Hyperplexed/pen/wvmvqmx"
+                                }
+                            ]}
+                        />
+                    </div>
+                </ComponentDisplay>
             </div>
-        </ComponentDisplay>
+        </div>
     )
 }
 
